1bfce68088eb09bd85683acfd387d5185334525d
Lapo Luchini <lapo@lapo.it>
2024-11-22 00:21:11 +0100

    Sort together, filter later.

--- packages/bruno-app/src/utils/collections/order.js
+++ packages/bruno-app/src/utils/collections/order.js
@@ -1,20 +1,19 @@
-import filter from 'lodash/filter';
 import { isItemAFolder, isItemARequest } from 'utils/collections';
 
-// we need to sort request items by seq property
-const sortRequestItems = (items = []) => {
-  return items.sort((a, b) => a.seq - b.seq);
-};
-
-// we need to sort folder items by name alphabetically
-const sortFolderItems = (items = []) => {
-  console.log('Coll item folder:', items);
-  return items.sort((a, b) => a.name.localeCompare(b.name));
-};
-
 export const getSortedItems = (collection, item) => {
-  const requestItems = sortRequestItems(filter(item.items, i => isItemARequest(i)));
-  const folderItems = sortFolderItems(filter(item.items, i => isItemAFolder(i)));
+  if (!('items' in item)) {
+    return { folderItems: [], requestItems: [] };
+  }
+
+  const items = item.items.toSorted((a, b) => {
+    if ('seq' in a || 'seq' in b) {
+      return +a.seq - +b.seq;
+    }
+    return a.name.localeCompare(b.name);
+  });
+
+  const requestItems = items.filter(i => isItemARequest(i));
+  const folderItems = items.filter(i => isItemAFolder(i));
 
   return { folderItems, requestItems };
 };
